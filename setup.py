efa496f5ddd51da42f508da6840eb803838a47d7
Hikmat <hdhamee@deerwalk.com>
2014-05-07 19:52:00 +0545

    udpate license

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
   description = 'Python based hadoop command-line interface',
   author = 'Hikmat Dhamee',
   author_email = 'me.hemant.available@gmail.com',
+  license = "http://opensource.org/licenses/MIT",
+  platforms = ["any"],
   url = 'https://github.com/mehikmat/PyHadoop', # use the URL to the github repo
   download_url = 'https://github.com/mehikmat/PyHadoop/tarball/0.1', # I'll explain this in a second
   keywords = ['hadoop', 'python', 'command-line'], # arbitrary keywords
