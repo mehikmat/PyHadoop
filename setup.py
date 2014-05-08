c5e85da628450e9a7f1185521265d76e94f2f2ac
Hikmat <hdhamee@deerwalk.com>
2014-05-08 13:16:22 +0545

    add entrypoint in setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-# meta file for distribution
-from distutils.core import setup
+# meta file for project distribution
+import os, sys
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now
+# 1) we have a top level README.md file and 
+# 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
   name = 'hadoopy',
   packages = ['hadoopy'], # this must be the same as the name above
@@ -7,10 +17,22 @@ setup(
   description = 'Python based hadoop command-line interface',
   author = 'Hikmat Dhamee',
   author_email = 'me.hemant.available@gmail.com',
-  license = "http://opensource.org/licenses/MIT",
+  license = "MIT",
   platforms = ["any"],
   url = 'https://github.com/mehikmat/PyHadoop', # use the URL to the github repo
   download_url = 'https://github.com/mehikmat/PyHadoop/tarball/0.1', # I'll explain this in a second
   keywords = ['hadoop', 'python', 'command-line'], # arbitrary keywords
-  classifiers = [],
+  long_description=open('README.md').read(),
+  classifiers=[
+        "Development Status :: Alpha",
+        "Topic :: Utilities",
+        "License ::MIT License",
+    ],
+  entry_points={
+        "console_scripts": [
+            "hadoopy=hadoopy:main",
+            "hadoopy%s=hadoopy:main" % sys.version[:1],
+            "hadoopy%s=hadoopy:main" % sys.version[:3],
+        ],
+    },
 )
