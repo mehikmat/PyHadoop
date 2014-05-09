331456cfeecafd6c90532459fa49e0b7221ba4e5
Hikmat Dhamee <mehikmat@users.noreply.github.com>
2014-05-08 18:07:43 -0700

    rename hadoopy to pyhadoop

--- pyhadoop/__main__.py
+++ pyhadoop/__main__.py
@@ -2,11 +2,10 @@ import sys
 
 if __package__ == '':
     import os
-    print ("inside main__pack")  
     path = os.path.dirname(os.path.dirname(__file__))
     sys.path.insert(0, path)
 
-import hadoopy
+import pyhadoop
 
 if __name__ == '__main__':    
-    sys.exit(hadoopy.main())
+    sys.exit(pyhadoop.main())
