efdee43cccabecb638ac3e49bd51c932b706011a
Hikmat Dhamee <mehikmat@users.noreply.github.com>
2015-02-05 17:45:00 +0545

    Update resources.py

--- pyhadoop/resources.py
+++ pyhadoop/resources.py
@@ -1,7 +1,7 @@
 # command map
 command_map = {
-	"start"  : "$HADOOP_HOME/bin/start-all.sh",
-	"stop"   : "$HADOOP_HOME/bin/stop-all.sh",
+	"start"  : "$HADOOP_HOME/sbin/start-all.sh",
+	"stop"   : "$HADOOP_HOME/sbin/stop-all.sh",
 	"cp"     : "$HADOOP_HOME/bin/hadoop fs -cp",
 	"rm"     : "$HADOOP_HOME/bin/hadoop fs -rmr",
 	"ls"     : "$HADOOP_HOME/bin/hadoop fs -ls",
